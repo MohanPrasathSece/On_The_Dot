85b0d4fb4cacc9d93456c3d64b5120b8410cf626
MohanPrasathSece <mohanprasath.s2023ece@sece.ac.in>
2025-12-10 14:06:32 +0530

    new

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,8 +14,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        sans: ["Inter", "system-ui", "sans-serif"],
-        display: ["Spectral", "Georgia", "serif"],
+        sans: ["Poppins", "system-ui", "sans-serif"],
+        display: ["Inter", "Poppins", "system-ui", "sans-serif"],
       },
       colors: {
         border: "hsl(var(--border))",
